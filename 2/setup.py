34cdeb9249c7b50d63c3ab99c296ec600f9dfc07
João Conde <joaodiasconde@gmail.com>
2022-04-16 17:27:39 +0100

    feat: initial structure

---
+++ 2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name = "postcodes",
+    version = "1.0.0",
+    author = "João Conde",
+    author_email = "joaodiasconde@gmail.com",
+    description = "Postcode utilities",
+    keywords = "postcodes shipping",
+    test_suite = "test",
+    classifiers = [
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7"
+    ]
+)
---
+++ 2/test/basic.py
@@ -0,0 +1,5 @@
+import unittest
+
+class BasicTest(unittest.TestCase):
+    def test_basic(self):
+        self.assertTrue(False)
