2ada6ffaf634aba5a20e4ba8477362836e6ff233
João Conde <joaodiasconde@gmail.com>
2022-04-16 17:12:37 +0100

    fix: fizzbuzz

--- 1/main.py
+++ 1/main.py
@@ -2,6 +2,6 @@ for i in range(1, 101):
     mul_three = i % 3 == 0
     mul_five = i % 5 == 0
     message = str(i) if not mul_three and not mul_five else ""
-    message = "Three" if mul_three else message
+    message += "Three" if mul_three else ""
     message += "Five" if mul_five else ""
     print(message)
