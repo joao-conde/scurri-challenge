5e76371be7d07a2a1807621f7241a08c4260b45b
João Conde <joaodiasconde@gmail.com>
2022-04-23 13:24:58 +0100

    feat: test utils mixin

---
+++ 2/src/postcode/test/utils.py
@@ -0,0 +1,9 @@
+import unittest
+
+class TestUtilsMixin(unittest.TestCase):
+    def assertNotRaises(self, error_type, func):
+        try:
+            func()
+        except error_type:
+            raise AssertionError("%s not raised by %s" % (error_type, func))
+        
