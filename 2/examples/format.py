765c2cbec466c4013ad619daddbc25c94e6c9c1a
João Dias Conde Azevedo <joaodiasconde@gmail.com>
2022-04-23 16:54:44 +0100

    feat: simplify example

--- 2/examples/format.py
+++ 2/examples/format.py
@@ -3,13 +3,6 @@ from postcodes import Country, Postcode
 code = "L 1 8J  Q"
 example = dict(
     code = code,
-    is_valid = Postcode.is_valid(Country.UNITED_KINGDOM, code)
-)
-print(example)
-
-formatted = Postcode.format(Country.UNITED_KINGDOM, code)
-example = dict(
-    formatted = formatted,
-    is_valid = Postcode.is_valid(Country.UNITED_KINGDOM, formatted)
+    formatted = Postcode.format(Country.UNITED_KINGDOM, code)
 )
 print(example)
